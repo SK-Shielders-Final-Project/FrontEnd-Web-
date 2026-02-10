2218128791dfa04228f2a9962d4a9de52d5e1226
jungmin <wjd1als2dl3@naver.com>
2026-02-10 16:39:40 +0900

    ㄹㅇㄹㅇ 찐  수정

--- src/admin/pages/OtpVerificationPage.js
+++ src/admin/pages/OtpVerificationPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { setCookie } from '../utils/cookie';
 
 const OtpVerificationPage = () => {
     const [otp, setOtp] = useState('');
